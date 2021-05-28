56fdb1f58d39384372b3ac4160673541bb20c398
Laszlo Harsanyi <laszlo.harsanyi@kpmg.hu>
2021-05-28 09:34:10 +0200

    End of Section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+namespace DatingApp.API.Data
+{
+    using API.Entities;
+    using Microsoft.EntityFrameworkCore;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
